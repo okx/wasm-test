0f2d9ae2b580d49e9a66eafa366bf54b302298ea
xingqiang.yuan <xingqiang.yuan@okg.com>
2023-01-18 16:15:49 +0800

    update migrate

--- scripts/cw20_migrate.sh
+++ scripts/cw20_migrate.sh
@@ -43,4 +43,41 @@ then
   exit 1
 fi;
 
+admin=$(exchaincli query wasm contract $cw20contractAddr $QUERY_EXTRA | jq '.contract_info.admin' | sed 's/\"//g')
+if [[ $admin != $captain ]]; then
+  echo "unexpected admin: $admin"
+  exit 1
+fi
+
+res=$(exchaincli tx gov submit-proposal migrate-contract "$cw20contractAddr" "$code_id" "{}" --deposit=100okt --title "migration" --description "migrate cw20_base" --from captain $TX_EXTRA)
+proposal_id=$(echo "$res" | jq '.logs[0].events[1].attributes[1].value' | sed 's/\"//g')
+echo "proposal_id: $proposal_id"
+res=$(exchaincli tx gov vote "$proposal_id" yes --from captain $TX_EXTRA)
+admin=$(exchaincli query wasm contract $cw20contractAddr $QUERY_EXTRA | jq '.contract_info.admin' | sed 's/\"//g')
+if [[ $admin != "" ]]; then
+  echo "unexpected admin: $admin"
+  exit 1
+fi
+
+res=$(exchaincli tx wasm instantiate "${code_id}" '{"decimals":10,"initial_balances":[{"address":"'$captain'","amount":"100"}],"name":"my test token", "symbol":"mtt"}' --label test1 --admin "$captain" --from captain $TX_EXTRA)
+cw20contractAddr2=$(echo "$res" | jq '.logs[0].events[0].attributes[0].value' | sed 's/\"//g')
+echo "contract address2: $cw20contractAddr2"
+
+admin=$(exchaincli query wasm contract $cw20contractAddr2 $QUERY_EXTRA | jq '.contract_info.admin' | sed 's/\"//g')
+if [[ $admin != $captain ]]; then
+  echo "unexpected admin: $admin"
+  exit 1
+fi
+
+
+res=$(exchaincli tx gov submit-proposal update-wasm-contract-method-blocked-list "$cw20contractAddr2" "transfer" --deposit 100okt --title "blacklist" --description "block transfer method of cw20_base contract" --from captain $TX_EXTRA)
+proposal_id=$(echo "$res" | jq '.logs[0].events[1].attributes[1].value' | sed 's/\"//g')
+echo "proposal_id: $proposal_id"
+res=$(exchaincli tx gov vote "$proposal_id" yes --from captain $TX_EXTRA)
+admin=$(exchaincli query wasm contract $cw20contractAddr $QUERY_EXTRA | jq '.contract_info.admin' | sed 's/\"//g')
+if [[ $admin != "" ]]; then
+  echo "unexpected admin: $admin"
+  exit 1
+fi
+
 echo "succeed~"
