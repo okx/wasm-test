c373f2f4a4f53e8d4138ae8c422f16a4a5efcc51
zhangkai <zhangkai.gis@163.com>
2023-04-21 11:24:34 +0800

    adapt testcase_0x to okb

---
+++ scripts/testcase_0x_okb.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+
+source ./localnet-prepare-okb.sh
+
+. ./utils.sh
+contract_dir=${PWD}/../contract
+check_file_exit $contract_dir
+
+QUERY_EXTRA="--node=$NODE"
+TX_EXTRA_UNBLOCKED="--fees 0.01okb --gas 3000000 --chain-id=$CHAIN_ID --node $NODE -b async -y"
+TX_EXTRA="--fees 0.01okb --gas 3000000 --chain-id=$CHAIN_ID --node $NODE -b block -y"
+
+temp=$(okbchaincli keys add --recover captain -m "puzzle glide follow cruel say burst deliver wild tragic galaxy lumber offer" -y)
+temp=$(okbchaincli keys add --recover admin17 -m "antique onion adult slot sad dizzy sure among cement demise submit scare" -y)
+temp=$(okbchaincli keys add --recover admin18 -m "lazy cause kite fence gravity regret visa fuel tone clerk motor rent" -y)
+captain_0x=$(okbchaincli keys show captain | jq -r '.eth_address')
+admin18_0x=$(okbchaincli keys show admin18 | jq -r '.eth_address')
+admin17_0x=$(okbchaincli keys show admin17 | jq -r '.eth_address')
+proposal_deposit="100okb"
+
+# usage:
+#   proposal_vote {proposal_id}
+proposal_vote() {
+  ./vote_okb.sh $1 $CHAIN_ID
+}
+
+#####################################################
+########    rest deployment whitelist     #########
+#####################################################
+echo "## rest wasm code deployment whitelist"
+res=$(okbchaincli tx gov submit-proposal update-wasm-deployment-whitelist "nobody" --deposit ${proposal_deposit} --title "test title" --description "test description" --from captain $TX_EXTRA)
+proposal_id=$(echo "$res" | jq '.logs[0].events[1].attributes[1].value' | sed 's/\"//g')
+echo "proposal_id: $proposal_id"
+proposal_vote "$proposal_id"
+
+#####################################################
+#############       store code       ################
+#####################################################
+
+echo "## store...everybody nobody"
+res=$(okbchaincli tx wasm store $contract_dir/cw20-base/artifacts/cw20_base.wasm --instantiate-everybody=true --from captain $TX_EXTRA)
+raw_log=$(echo "$res" | jq '.raw_log' | sed 's/\"//g')
+failed_log_prefix="unauthorized: Failed to create code, nobody allowed to upload contract: failed to execute message; message index: 0"
+if [[ "${raw_log}" != "${failed_log_prefix}" ]];
+then
+  echo "expect fail when instantiate contract with invalid amount"
+  exit 1
+fi;
+
+
+echo "## store...only address"
+res=$(okbchaincli tx wasm store $contract_dir/cw20-base/artifacts/cw20_base.wasm --instantiate-only-address="$admin17_0x" --from captain $TX_EXTRA)
+raw_log=$(echo "$res" | jq '.raw_log' | sed 's/\"//g')
+failed_log_prefix="unauthorized: Failed to create code, nobody allowed to upload contract: failed to execute message; message index: 0"
+if [[ "${raw_log}" != "${failed_log_prefix}" ]];
+then
+  echo "expect fail when instantiate contract with invalid amount"
+  exit 1
+fi;
+
+#####################################################
+########    update deployment whitelist     #########
+#####################################################
+echo "## update special address deployment whitelist"
+res=$(okbchaincli tx gov submit-proposal update-wasm-deployment-whitelist "$captain_0x,$admin18_0x" --deposit ${proposal_deposit} --title "test title" --description "test description" --from captain $TX_EXTRA)
+proposal_id=$(echo "$res" | jq '.logs[0].events[1].attributes[1].value' | sed 's/\"//g')
+echo "proposal_id: $proposal_id"
+proposal_vote "$proposal_id"
+
+echo "## store...everybody special address"
+res=$(okbchaincli tx wasm store $contract_dir/cw20-base/artifacts/cw20_base.wasm --instantiate-everybody=true --from admin17 $TX_EXTRA)
+raw_log=$(echo "$res" | jq '.raw_log' | sed 's/\"//g')
+failed_log_prefix="unauthorized: Failed to create code, you are not allowed to upload contract as you are not on the authorized list: failed to execute message; message index: 0"
+if [[ "${raw_log}" != "${failed_log_prefix}" ]];
+then
+  echo "expect fail when instantiate contract with invalid amount"
+  exit 1
+fi;
+
+res=$(okbchaincli tx wasm store $contract_dir/cw20-base/artifacts/cw20_base.wasm --instantiate-everybody=true --from captain $TX_EXTRA)
+cw20_code_id1=$(echo "$res" | jq '.logs[0].events[1].attributes[0].value' | sed 's/\"//g')
+
+
+echo "## store...only special address"
+res=$(okbchaincli tx wasm store $contract_dir/cw20-base/artifacts/cw20_base.wasm --instantiate-only-address="$admin17_0x" --from admin17 $TX_EXTRA)
+raw_log=$(echo "$res" | jq '.raw_log' | sed 's/\"//g')
+failed_log_prefix="unauthorized: Failed to create code, you are not allowed to upload contract as you are not on the authorized list: failed to execute message; message index: 0"
+if [[ "${raw_log}" != "${failed_log_prefix}" ]];
+then
+  echo "expect fail when instantiate contract with invalid amount"
+  exit 1
+fi;
+
+res=$(okbchaincli tx wasm store $contract_dir/cw20-base/artifacts/cw20_base.wasm --instantiate-only-address="$admin17_0x" --from captain $TX_EXTRA)
+cw20_code_id3=$(echo "$res" | jq '.logs[0].events[1].attributes[0].value' | sed 's/\"//g')
+
+#####################################################
+#########    instantiate contract      ##############
+#####################################################
+
+echo "## instantiate everybody..."
+res=$(okbchaincli tx wasm instantiate "$cw20_code_id1" '{"decimals":10,"initial_balances":[{"address":"'$captain_0x'","amount":"100000000"}],"name":"my test token", "symbol":"mtt"}' --label test1 --admin "$captain_0x" --from captain $TX_EXTRA)
+echo "instantiate cw20 succeed"
+echo $res | jq -r
+if [[ $(echo "$res" | jq '.logs[0].events[0].attributes[0].key' | sed 's/\"//g') != "_contract_address" ]];
+then
+  echo "unexpected result of instantiate"
+  exit 1
+fi;
+
+#####################################################
+########    rest deployment whitelist     #########
+#####################################################
+echo "## rest wasm code deployment whitelist"
+res=$(okbchaincli tx gov submit-proposal update-wasm-deployment-whitelist "nobody" --deposit ${proposal_deposit} --title "test title" --description "test description" --from captain $TX_EXTRA)
+proposal_id=$(echo "$res" | jq '.logs[0].events[1].attributes[1].value' | sed 's/\"//g')
+echo "proposal_id: $proposal_id"
+proposal_vote "$proposal_id"
+
+echo "all cases succeed~"
